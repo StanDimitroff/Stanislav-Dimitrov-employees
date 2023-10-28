5a147adcbe8acccaefdfa8b898f4101bc66ecf26
standimitroff <standimitroff@gmail.com>
2023-10-28 11:33:09 +0300

    Create `SwiftUI` based application with Unit tests

---
+++ Employees/Employees/EmployeesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesApp.swift
+//  Employees
+//
+//  Created by Stanislav Dimitrov on 28.10.23.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

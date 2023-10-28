0a5f33f6705819d896b1447bd48594591d0f40e1
standimitroff <standimitroff@gmail.com>
2023-10-28 19:42:45 +0300

    Load sample data extracts single pair of employees with common projects

--- Employees/EmployeesTests/EmployeesTests.swift
+++ Employees/EmployeesTests/EmployeesTests.swift
@@ -13,7 +13,30 @@ struct Employee {
   let projects: [Int]
 }
 
+class EmployeeMatcher {
+  func employeesWithCommonProjects(employees: [Employee]) -> [Employee] {
+    let projectIDs = employees.flatMap { $0.projects }
+    let groups = Dictionary(grouping: projectIDs, by: { $0 })
+    let duplicates: [Int] = Array(groups.filter { $1.count > 1 }.keys)
+
+    let filtered = employees.filter { $0.projects.contains(duplicates) }
+
+    return filtered
+  }
+}
+
 final class EmployeesTests: XCTestCase {
+
+  func testLoadDataExtractsEmployeePairWithCommonProjects() {
+    let employees = testEmployees()
+    let sut = EmployeeMatcher()
+
+    let employeesWithCommonProjects = sut.employeesWithCommonProjects(employees: employees)
+
+    XCTAssertEqual(employeesWithCommonProjects.count, 2)
+    XCTAssertEqual(employeesWithCommonProjects[0].id, 143)
+    XCTAssertEqual(employeesWithCommonProjects[1].id, 113)
+  }
 
   private func testEmployees() -> [Employee] {
     [
@@ -21,8 +44,7 @@ final class EmployeesTests: XCTestCase {
       .init(id: 218, projects: [12, 9]),
       .init(id: 113, projects: [10, 17]),
       .init(id: 67, projects: [33]),
-      .init(id: 218, projects: [9])
+      .init(id: 55, projects: [8])
     ]
   }
 }
-

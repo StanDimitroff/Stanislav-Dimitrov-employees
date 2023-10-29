2cc2361e10a7853ea2629eb506f6847aeb5dabed
standimitroff <standimitroff@gmail.com>
2023-10-29 15:42:09 +0200

    Parse CSV data returns collection of `Record` instances

--- Employees/EmployeesTests/CSVParserTests.swift
+++ Employees/EmployeesTests/CSVParserTests.swift
@@ -6,30 +6,64 @@
 //
 
 import XCTest
+@testable import Employees
 
 class CSVParser {
+
+  let dateProcessor = DateProcessor()
+
   func loadFile(from url: URL) -> String? {
     let csvData = try? String(contentsOf: url, encoding: .utf8)
-    
+
     return csvData
+  }
+
+  func parse(csvData: String) -> [Record] {
+    let rows = csvData.components(separatedBy: "\n")
+
+    var records = [Record]()
+
+    for row in rows {
+      let columns = row.components(separatedBy: ", ")
+
+      let employeeId = Int(columns[0]) ?? 0
+      let projectId = Int(columns[1]) ?? 0
+      let dateFrom = columns[2]
+      let dateTo = columns[3].replacingOccurrences(of: "NULL", with: "")
+
+      let dateInterval = dateProcessor.dateIntervalFor(start: dateFrom, end: dateTo)
+
+      let record = Record(employeeId: employeeId, projectId: projectId, dateInterval: dateInterval)
+      records.append(record)
+    }
+
+    return records
   }
 }
 
 final class CSVParserTests: XCTestCase {
 
   func testLoadFileFromURLReturnsFileData() {
-    let bundle = Bundle(for: CSVParserTests.self)
-
-    guard let path = bundle.path(forResource: "records", ofType: "csv") else {
-      XCTFail("Cannot file find path")
-      return
-    }
-
-    let fileURL = URL(fileURLWithPath: path)
     let sut = CSVParser()
 
-    let csvData = sut.loadFile(from: fileURL)
+    let csvData = sut.loadFile(from: fileURL())
 
     XCTAssertNotNil(csvData)
+  }
+
+  func testParseCSVDataReturnsRecords() {
+    let sut = CSVParser()
+    let csvData = sut.loadFile(from: fileURL())!
+
+    let actualRecords = sut.parse(csvData: csvData)
+
+    XCTAssertEqual(actualRecords.count, 3)
+  }
+
+  private func fileURL() -> URL {
+    let bundle = Bundle(for: CSVParserTests.self)
+    let path = bundle.path(forResource: "records", ofType: "csv")!
+
+    return URL(fileURLWithPath: path)
   }
 }

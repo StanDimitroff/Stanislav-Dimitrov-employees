510dc7b871bbbec6c02204d6b66e75c79e1b7790
standimitroff <standimitroff@gmail.com>
2023-10-29 13:22:57 +0200

    Load data extracts record pairs with common projects and period intersection

--- Employees/EmployeesTests/EmployeesTests.swift
+++ Employees/EmployeesTests/EmployeesTests.swift
@@ -15,11 +15,24 @@ struct Record: Equatable {
 }
 
 class RecordMatcher {
-  func recordsWithCommonProjects(records: [Record]) -> [Int: [Record]] {
-    let groupsByProject = Dictionary(grouping: records, by: { $0.projectId })
-    let groupsWithMoreThanOneValue = groupsByProject.filter { $0.value.count > 1 }
+  func recordsWithCommonProjectsForPeriod(records: [Record]) -> [Int: [Record]] {
+    let recordGroupsByProject = Dictionary(grouping: records, by: { $0.projectId })
+    let recordGroupsWithMoreThanOneProject = recordGroupsByProject.filter { $0.value.count > 1 }
 
-    return groupsWithMoreThanOneValue
+    let isPairWorkedAtTheSameTime: ([Record]) -> Bool = { records in
+      guard records.count > 1 else { return false }
+
+      return records[0].dateInterval.intersects(records[1].dateInterval)
+    }
+
+    var filteredByPeriodIntersection = [Int: [Record]]()
+    for (key, value) in recordGroupsWithMoreThanOneProject {
+      if isPairWorkedAtTheSameTime(value) {
+        filteredByPeriodIntersection[key] = value
+      }
+    }
+
+    return filteredByPeriodIntersection
   }
 }
 
@@ -45,7 +58,7 @@ final class EmployeesTests: XCTestCase {
 
   private lazy var dateIntervals = createDateIntervals()
 
-  func testLoadDataExtractsRecordPairWithCommonProjects() {
+  func testLoadDataExtractsRecordPairsWithCommonProjectsForPeriod() {
     let records = [
       Record(employeeId: 143, projectId: 10, dateInterval: dateInterval(at: 0)),
       Record(employeeId: 333, projectId: 12, dateInterval: dateInterval(at: 3)),
@@ -57,19 +70,14 @@ final class EmployeesTests: XCTestCase {
     let sut = makeSUT()
 
     let expectedRecords = [
-      10:
-      [
-        Record(employeeId: 143, projectId: 10, dateInterval: dateInterval(at: 0)),
-        Record(employeeId: 113, projectId: 10, dateInterval: dateInterval(at: 4))
-      ],
       12:
-      [
-        Record(employeeId: 333, projectId: 12, dateInterval: dateInterval(at: 3)),
-        Record(employeeId: 218, projectId: 12, dateInterval: dateInterval(at: 2)),
-      ]
+        [
+          Record(employeeId: 333, projectId: 12, dateInterval: dateInterval(at: 3)),
+          Record(employeeId: 218, projectId: 12, dateInterval: dateInterval(at: 2)),
+        ]
     ]
 
-    let actualRecords = sut.recordsWithCommonProjects(records: records)
+    let actualRecords = sut.recordsWithCommonProjectsForPeriod(records: records)
 
     XCTAssertEqual(expectedRecords, actualRecords)
   }
@@ -81,9 +89,9 @@ final class EmployeesTests: XCTestCase {
   private func createDateIntervals() -> [DateInterval] {
     let processor = DateProcessor()
     let dates = [
-      (start: "2012-04-16", end: ""),
+      (start: "2009-01-01", end: "2011-04-27"),
       (start: "2012-05-18", end: "2012-05-20"),
-      (start: "2011-06-21", end: "2017-04-06"),
+      (start: "2012-05-16", end: ""),
       (start: "2015-03-12", end: "2015-05-11"),
       (start: "2019-01-18", end: "2022-02-19"),
       (start: "2010-07-14", end: "2013-09-29"),

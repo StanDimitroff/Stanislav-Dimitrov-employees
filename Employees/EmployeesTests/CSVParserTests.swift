17860005e4f4e177d6b852e821a74b14de55d2e3
standimitroff <standimitroff@gmail.com>
2023-10-29 15:44:16 +0200

    Move `CSVParser` to a separate file

--- Employees/EmployeesTests/CSVParserTests.swift
+++ Employees/EmployeesTests/CSVParserTests.swift
@@ -8,39 +8,6 @@
 import XCTest
 @testable import Employees
 
-class CSVParser {
-
-  let dateProcessor = DateProcessor()
-
-  func loadFile(from url: URL) -> String? {
-    let csvData = try? String(contentsOf: url, encoding: .utf8)
-
-    return csvData
-  }
-
-  func parse(csvData: String) -> [Record] {
-    let rows = csvData.components(separatedBy: "\n")
-
-    var records = [Record]()
-
-    for row in rows {
-      let columns = row.components(separatedBy: ", ")
-
-      let employeeId = Int(columns[0]) ?? 0
-      let projectId = Int(columns[1]) ?? 0
-      let dateFrom = columns[2]
-      let dateTo = columns[3].replacingOccurrences(of: "NULL", with: "")
-
-      let dateInterval = dateProcessor.dateIntervalFor(start: dateFrom, end: dateTo)
-
-      let record = Record(employeeId: employeeId, projectId: projectId, dateInterval: dateInterval)
-      records.append(record)
-    }
-
-    return records
-  }
-}
-
 final class CSVParserTests: XCTestCase {
 
   func testLoadFileFromURLReturnsFileData() {

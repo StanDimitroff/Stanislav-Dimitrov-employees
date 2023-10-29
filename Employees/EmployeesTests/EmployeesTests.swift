3afa57a778c295a875119bc4b966a69314c90554
standimitroff <standimitroff@gmail.com>
2023-10-29 09:40:57 +0200

    Replace `Employee` model  with `Record` and simplify matching logic

--- Employees/EmployeesTests/EmployeesTests.swift
+++ Employees/EmployeesTests/EmployeesTests.swift
@@ -8,73 +8,99 @@
 import XCTest
 @testable import Employees
 
-struct Employee: Equatable {
-  let id: Int
-  let projects: [Int]
+struct Record: Equatable {
+  let employeeId: Int
+  let projectId: Int
+  let dateInterval: DateInterval
 }
 
-class EmployeeMatcher {
-  func employeesWithCommonProjects(employees: [Employee]) -> [Employee] {
-    let projectIDs: [Int] = employees.flatMap { $0.projects }
-    let groups = Dictionary(grouping: projectIDs, by: { $0 })
-    let duplicates: [Int] = Array(groups.filter { $1.count > 1 }.keys)
+class RecordMatcher {
+  func recordsWithCommonProjects(records: [Record]) -> [Int: [Record]] {
+    let groupsByProject = Dictionary(grouping: records, by: { $0.projectId })
+    let groupsWithMoreThanOneValue = groupsByProject.filter { $0.value.count > 1 }
 
-    let filtered = employees.filter { $0.projects.contains { element in
-      return duplicates.contains(element)
-    }}
+    return groupsWithMoreThanOneValue
+  }
+}
 
-    return filtered
+class DateProcessor {
+
+  private lazy var dateFormatter = DateFormatter()
+
+  func dateIntervalFor(start: String, end: String) -> DateInterval {
+    let startDate = dateFrom(string: start)
+    let endDate = dateFrom(string: end)
+
+    return DateInterval(start: startDate, end: endDate)
+  }
+
+  func dateFrom(string: String) -> Date {
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+
+    return dateFormatter.date(from: string) ?? Date()
   }
 }
 
 final class EmployeesTests: XCTestCase {
 
-  func testLoadDataExtractsEmployeePairWithCommonProjects() {
-    let employees = testEmployeesSinglePair()
+  private lazy var dateIntervals = createDateIntervals()
+
+  func testLoadDataExtractsRecordPairWithCommonProjects() {
+    let records = [
+      Record(employeeId: 143, projectId: 10, dateInterval: dateInterval(at: 0)),
+      Record(employeeId: 333, projectId: 12, dateInterval: dateInterval(at: 3)),
+      Record(employeeId: 113, projectId: 10, dateInterval: dateInterval(at: 4)),
+      Record(employeeId: 218, projectId: 12, dateInterval: dateInterval(at: 2)),
+      Record(employeeId: 111, projectId: 9, dateInterval: dateInterval(at: 5))
+    ]
+
     let sut = makeSUT()
-    let expectedEmployees = [Employee(id: 143, projects: [10, 22]), Employee(id: 113, projects: [10, 17])]
 
-    let actualEmployees = sut.employeesWithCommonProjects(employees: employees)
+    let expectedRecords = [
+      10:
+      [
+        Record(employeeId: 143, projectId: 10, dateInterval: dateInterval(at: 0)),
+        Record(employeeId: 113, projectId: 10, dateInterval: dateInterval(at: 4))
+      ],
+      12:
+      [
+        Record(employeeId: 333, projectId: 12, dateInterval: dateInterval(at: 3)),
+        Record(employeeId: 218, projectId: 12, dateInterval: dateInterval(at: 2)),
+      ]
+    ]
 
-    XCTAssertEqual(expectedEmployees, actualEmployees)
+    let actualRecords = sut.recordsWithCommonProjects(records: records)
+
+    XCTAssertEqual(expectedRecords, actualRecords)
   }
 
-    func testLoadDataExtractsMultipleEmployeePairsWithCommonProjects() {
-      let employees = testEmployeesMultiplePair()
-      let sut = makeSUT()
-      let expectedEmployees = [
-        Employee(id: 143, projects: [10, 22]), 
-        Employee(id: 218, projects: [12, 9]),
-        Employee(id: 113, projects: [10, 17]),
-        Employee(id: 55, projects: [8, 9])
-      ]
-
-      let actualEmployees = sut.employeesWithCommonProjects(employees: employees)
-
-      XCTAssertEqual(expectedEmployees, actualEmployees)
-    }
-
-  private func makeSUT() -> EmployeeMatcher {
+  private func makeSUT() -> RecordMatcher {
     return .init()
   }
 
-  private func testEmployeesSinglePair() -> [Employee] {
-    [
-      .init(id: 143, projects: [10, 22]),
-      .init(id: 218, projects: [12, 9]),
-      .init(id: 113, projects: [10, 17]),
-      .init(id: 67, projects: [33]),
-      .init(id: 55, projects: [8])
+  private func createDateIntervals() -> [DateInterval] {
+    let processor = DateProcessor()
+    let dates = [
+      (start: "2012-04-16", end: ""),
+      (start: "2012-05-18", end: "2012-05-20"),
+      (start: "2011-06-21", end: "2017-04-06"),
+      (start: "2015-03-12", end: "2015-05-11"),
+      (start: "2019-01-18", end: "2022-02-19"),
+      (start: "2010-07-14", end: "2013-09-29"),
+      (start: "2019-01-18", end: "2023-08-17"),
+      (start: "2008-06-15", end: "2010-11-18"),
+      (start: "2009-12-01", end: "2014-10-25")
     ]
+
+    var dateIntervals = [DateInterval]()
+    dates.forEach { date in
+      dateIntervals.append(processor.dateIntervalFor(start: date.start, end: date.end))
+    }
+
+    return dateIntervals
   }
 
-  private func testEmployeesMultiplePair() -> [Employee] {
-    [
-      .init(id: 143, projects: [10, 22]),
-      .init(id: 218, projects: [12, 9]),
-      .init(id: 113, projects: [10, 17]),
-      .init(id: 67, projects: [33]),
-      .init(id: 55, projects: [8, 9])
-    ]
+  private func dateInterval(at index: Int) -> DateInterval {
+    dateIntervals[index]
   }
 }
